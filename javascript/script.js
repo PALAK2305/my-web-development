36fffa03f7a986fd5bb3e3346fb5d6bb4407069a
PALAK2305 <damgudepalak@gmail.com>
2025-08-06 16:49:02 +0530

    Javascript

---
+++ javascript/script.js
@@ -0,0 +1,88 @@
+// unary operator
+// let a = 5;
+// let b = 2;
+
+// console.log("a=", a,"& b= ",b );
+
+// console.log("a++=", a++);
+// console.log(a);
+
+// assignment operator
+// let a = 5;
+// let b = 2;
+
+// a += 4; 
+// console.log(a);
+
+//comparison operator
+// let a = 5;
+// let b = 2;
+
+// console.log("5 !=2", a!=b);
+
+// // logical operators
+// let a = 5;
+// let b = 2;
+// console.log("!(5<2) =", !(a < b));
+
+// conditional statement
+// if statement
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// }
+// if (mode === "light") {
+//     color = "white";
+// }
+// console.log(color);
+
+// if-else
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// }
+// else {
+//     color = "white";
+// }  
+// console.log(color);
+
+// // conditional statement
+
+// let age = 17;
+
+// let result = age >= 18 ?  "adult" : "not adult";
+// console.log(result);  
+// let num = prompt("enter a number:");
+
+// if (num % 8 === 0) {
+//     console.log(num,"is a multiple of 5");
+// }
+// else {
+//     console.log(num,"is not a multiple of 5 ");
+// }
+
+// practice 
+// let score = prompt("enter a number:");
+// let grade;
+// if (score>= 90 && score<=100) {
+//     grade= "A";
+// }
+
+//  else if (score>= 70 && score<=89) {
+//     grade= "B";
+// }
+// else if (score>= 60 && score<=69) {
+//     grade= "C";
+// }
+// else if (score>= 50 && score<=59) {
+//     grade= "D";
+// }
+// if (score>= 0 && score<=49) {
+//     grade= "F";
+// }
+// console.log("Your grade according to your score is :" ,grade);
+
