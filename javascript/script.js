aafd9ba6fd8081bb4ba312192de0f5dae5adcc10
Palak Damgude <damgudepalak@gmail.com>
2025-08-07 20:56:32 +0530

    practice question

--- javascript/script.js
+++ javascript/script.js
@@ -66,23 +66,23 @@
 // }
 
 // practice 
-// let score = prompt("enter a number:");
-// let grade;
-// if (score>= 90 && score<=100) {
-//     grade= "A";
-// }
+let score = prompt("enter a number:");
+let grade;
+if (score>= 90 && score<=100) {
+    grade= "A";
+}
 
-//  else if (score>= 70 && score<=89) {
-//     grade= "B";
-// }
-// else if (score>= 60 && score<=69) {
-//     grade= "C";
-// }
-// else if (score>= 50 && score<=59) {
-//     grade= "D";
-// }
-// if (score>= 0 && score<=49) {
-//     grade= "F";
-// }
-// console.log("Your grade according to your score is :" ,grade);
+ else if (score>= 70 && score<=89) {
+    grade= "B";
+}
+else if (score>= 60 && score<=69) {
+    grade= "C";
+}
+else if (score>= 50 && score<=59) {
+    grade= "D";
+}
+if (score>= 0 && score<=49) {
+    grade= "F";
+}
+console.log("Your grade according to your score is :" ,grade);
 
