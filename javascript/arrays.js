df4d5c84128c3eee99cda8319e1daf4beb83ac8b
Palak Damgude <damgudepalak@gmail.com>
2025-09-07 21:08:32 +0530

    practice question

---
+++ javascript/arrays.js
@@ -0,0 +1,27 @@
+// let marks = ["98", "23", "45", "60"];
+// // for loop
+// for (let i=0; i<marks.length; i++){
+// console.log(marks[i]);
+// }
+
+// practice question (average)
+// let marks = [85,97,44,37,76,60];
+// let sum = 0;
+
+// for (let val of marks){
+// sum +=val ;    
+// }
+
+// let avg = sum / marks.length;
+// console.log(`avg marks of the class = ${avg}`);
+
+// practice question
+let prices = [250,645,300,900,50];
+
+ for( let val of items){
+   let finalprice =val/10;
+ }
+   console.log(`store final price after applying offer ${finalprice}`);
+
+
+
