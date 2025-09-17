03b297959ae236c7f468748553256c1913ed77c5
Palak Damgude <damgudepalak@gmail.com>
2025-09-18 00:33:31 +0530

    forEach loop

--- javascript/function_&_methods.js
+++ javascript/function_&_methods.js
@@ -29,34 +29,53 @@
 // };
 
 // practice question (return cont of vowels)
-function countvowels(str) {
-  let count = 0;
-  for (const char of str) {
-    if (
-      char === "a" ||
-      char === "e" ||
-      char === "i" ||
-      char === "o" ||
-      char === "u"
-    ) {
-      count++;
-    }
-  }
- return count;
-}
+// function countvowels(str) {
+//   let count = 0;
+//   for (const char of str) {
+//     if (
+//       char === "a" ||
+//       char === "e" ||
+//       char === "i" ||
+//       char === "o" ||
+//       char === "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//  return count;
+// }
 
-const countvow = (str) => {
-    let count = 0;
-  for (const char of str) {
-    if (
-      char === "a" ||
-      char === "e" ||
-      char === "i" ||
-      char === "o" ||
-      char === "u"
-    ) {
-      count++;
-    }
-  }
- return count;
-}
+// // in modern js style
+// const countvow = (str) => {
+//     let count = 0;
+//   for (const char of str) {
+//     if (
+//       char === "a" ||
+//       char === "e" ||
+//       char === "i" ||
+//       char === "o" ||
+//       char === "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//  return count;
+// }
+
+// foreach loop in arrays
+// arr.forEach(callBackFunction)
+// callbackfuntion: it is used to execute for each element in the Array
+// A callback is a function passed asan argument to another function.
+
+// let arr = [1,2,3,4,5];
+// arr.forEach(function printVal(val){
+//   console.log(val);
+// })
+
+// or
+
+let arr = [1,2,3,4,5];
+arr.forEach((val) => {
+  console.log(val);
+})
+
