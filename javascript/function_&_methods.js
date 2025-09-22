43cd46d825e2b4905178b991a9e93504d9da84eb
Palak Damgude <damgudepalak@gmail.com>
2025-09-23 00:04:45 +0530

    Dom.js

--- javascript/function_&_methods.js
+++ javascript/function_&_methods.js
@@ -127,19 +127,19 @@
 // console.log(selected);
 
 // practice question 2
-let n= prompt("Enter a number :");
-let arr = [];
-for(let i=1; i<=n; i++){
-  arr[i-1] = i;
-}
-console.log(arr);
+// let n= prompt("Enter a number :");
+// let arr = [];
+// for(let i=1; i<=n; i++){
+//   arr[i-1] = i;
+// }
+// console.log(arr);
 
-let sum = arr.reduce((prev,curr) => {
-  return prev+curr;
-});
-console.log(sum);
+// let sum = arr.reduce((prev,curr) => {
+//   return prev+curr;
+// });
+// console.log(sum);
 
-let product = arr.reduce((prev,curr) => {
-  return prev*curr;
-});
-console.log(product);
+// let product = arr.reduce((prev,curr) => {
+//   return prev*curr;
+// });
+// console.log(product);
