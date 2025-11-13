3693d8e9e7a397de9161d2f2db812e81b3e708f2
Palak Damgude <damgudepalak@gmail.com>
2025-11-13 11:59:59 +0530

    practice

--- javascript/events.js
+++ javascript/events.js
@@ -6,14 +6,16 @@
 // form events (submit etc)
 // print Event & many more
 
-let btn1 = document.querySelector("#btn1");
+// Event object - it is a special object that has details about the Event. All Event handlers have access to the Event Object's properties and methods
 
-btn1.onclick = (evt) => {
-    console.log(evt);
-    console.log(evt.type);
-    console.log(evt.target);
-    console.log(evt.clientX, evt.clientY);
-}
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = (evt) => {
+//     console.log(evt);
+//     console.log(evt.type);
+//     console.log(evt.target);
+//     console.log(evt.clientX, evt.clientY);
+// }
 
 // let box = document.querySelector("div");
 
@@ -21,4 +23,44 @@ btn1.onclick = (evt) => {
 //     console.log("you are inside div");
 // }
 
-// Event object - it is a special object that has details about the Event. All Event handlers have access to the Event Object's properties and methods
+// Event listeners -: node.addEventListener(event,callback)
+
+// let btn1= document.querySelector("#btn1");
+
+// btn1.addEventListener("click", (evt) => {
+//     console.log(evt);
+//     console.log("button 1 was clicked");
+// });
+
+// btn1.addEventListener("click", () => {
+//     console.log("button 2  was clicked");
+// });
+
+// btn1.addEventListener("click", () => {
+//     console.log("button 4  was clicked");
+// });
+
+// const handler3=  () => {
+//     console.log("button 3  was clicked");
+// }
+// btn1.removeEventListener("click",handler3);
+
+// practice question (toggle button dark and white mode)
+// let modebtn = document.querySelector("#mode");
+// let body = document.querySelector("body");
+// let currmode = "light"; //dark
+
+// modebtn.addEventListener("click", ()=>{
+//     if(currmode=="light"){
+//        currmode= "dark";
+//        body.classList.add("dark");
+//        body.classList.remove("light");
+//     }
+//     else{
+//         currmode ="light";
+//         body.classList.add("light");
+//         body.classList.remove("dark");
+
+//     }
+//     console.log(currmode);
+// });
