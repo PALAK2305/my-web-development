7f85186e7951489263d986ac050d8362a1ccb1f9
Palak Damgude <damgudepalak@gmail.com>
2025-11-22 22:05:36 +0530

    asyn

--- javascript/callbacks_promises.js
+++ javascript/callbacks_promises.js
@@ -40,7 +40,7 @@
 
 // eg
 // function getdata(dataid, getnextdata) {
-  //2s
+//2s
 //   setTimeout(() => {
 //     console.log("data", dataid);
 //     if (getnextdata) {
@@ -62,7 +62,6 @@
 //   });
 // });
 
-
 // Promises - Promise is for "eventual" completion of task. it is an object in JS.
 // It is a solution to callback hell.
 // let promise = new Promise ((resolve,reject) => {....})
@@ -73,7 +72,6 @@
 //     console.log("I am a Promise");
 //     resolve(123);
 // });
-
 
 // eg2
 // function getdata(dataid, getnextdata) {
@@ -105,7 +103,6 @@
 //     console.log("Promise fulfilled");
 // });
 
-
 // eg2
 // const getPromise = () => {
 //     return new Promise((resolve, reject) => {
@@ -123,11 +120,95 @@
 //     console.log("rejected");
 // });
 
-function asyncFunc() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-        console.log("some data1");
-        resolve("success");
-        }, 3000);
-    });
-};
+// promise chaining
+
+// eg1
+// function asyncFunc1() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("some data1");
+//       resolve("success");
+//     }, 3000);
+//   });
+// }
+
+// function asyncFunc2() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("some data2");
+//       resolve("success");
+//     }, 6000);
+//   });
+// }
+
+// console.log("fetching data1");
+//  asyncFunc1().then((res) => {
+//   console.log("fetching data2");
+//  asyncFunc2().then((res) => {
+//   });
+// });
+
+// eg2
+
+// function getdata(dataid) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("data", dataid);
+//       resolve("success");
+//     }, 2000);
+//   });
+// }
+
+//promise chain
+//     getdata(1)
+//     .then((res) => {
+//     return getdata(2);
+//     })
+//     .then((res) => {
+//         return getdata(3);
+//     })
+//     .then((res) => {
+//      console.log("success");
+//   });
+
+// Async-Await -: async function always return a Promise.
+// async function myFunc() {...}
+// await pauses the execution of its surrounding async function until the promise is settled.
+
+// eg1
+// async function hello() {
+//     console.log("hello")
+// }
+
+// eg2
+// function api() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("weather data");
+//       resolve(200);
+//     }, 2000);
+//   });
+// }
+
+// async function getweatherdata() {
+//     await api();
+//     await api();
+// }
+
+// eg3
+ function getdata(dataid) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data", dataid);
+      resolve("success");
+    }, 2000);
+  });
+}
+
+// async-await
+async function getalldata() {
+    await getdata(1);
+    await getdata(2);
+    await getdata(3);
+}
+
